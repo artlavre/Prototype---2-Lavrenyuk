27c2393c3bd81529f3c1c105f89195f7bbe3f390
artlavre <apt.lav@gmail.com>
2022-02-06 12:40:02 +0200

    Prototype-2-Completed

---
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward: MonoBehaviour
+{
+    public float speed = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if(transform.position.z > 30)
+        {
+            Destroy(gameObject);
+        }else if(transform.position.z < -10)
+        {
+            Debug.Log("lose");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnX = 15;
+    private float spawnZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", 2 , 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 0, spawnZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+    }
+}
